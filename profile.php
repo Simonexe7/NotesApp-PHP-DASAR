a5ae50ca774c72e2b1d7ba4f5c86f8d47b652375
Muhammad Farhan <farhansya.ban07@gmail.com>
2024-05-23 22:36:58 +0700

    memperbaiki halaman profile

--- profile.php
+++ profile.php
@@ -1,27 +1,42 @@
 <?php
 require_once 'templates/header.php';
-require_once 'config.php';
-
+require_once 'functions.php';
+session_start();
+$userid = $_SESSION["userid"];
+$username = $_SESSION["username"];
+$user = getUser($userid, $username);
 ?>
 <div class="container">
     <button type="button" class="btn-back" id="back" onclick="window.history.back()"><ion-icon
             name="chevron-back-outline"></ion-icon> Kembali</button>
     <div class="profile" id="profile">
         <div class="user-profile">
-            <img src="assets/imgs/customer01.jpg" alt="">
+            <img src="assets/imgs/<?= $user['profileImg'] ?>" alt="">
         </div>
 
         <div class="title-profile">
-            <h1>Your Profile</h1>
+            <h1><?= $user['firstname'] ?> <?= $user['lastname'] ?></h1>
             <button class="btn-edit-profile" id="btnEditProfile"><ion-icon name="create"></ion-icon> Edit
                 Profile</button>
         </div>
 
         <form action="" method="POST">
             <div class="add-input add-input-profile">
-                <input type="text" placeholder="Title" name="title" value="Username : " disabled>
-                <input type="text" placeholder="Tags" name="tags" value="Hobbies : " disabled>
-                <textarea type="text" placeholder="Body" name="body" disabled>Description : </textarea>
+                <input type="text" placeholder="Title" name="title" value="Username : <?= $user['username'] ?>"
+                    disabled>
+                <input type="email" placeholder="Email" name="email" value="Email : <?= $user['email'] ?>" disabled>
+                <input type="text" placeholder="Tags" name="tags" value="Hobbies : <?php
+                if ($user['hobbies'] == "") {
+                    echo "(Kosong)";
+                } else {
+                    echo $user['hobbies'];
+                } ?>" disabled>
+                <textarea type="text" placeholder="Body" name="body" disabled>Description : <?php
+                if ($user['description'] == "") {
+                    echo "(Kosong)";
+                } else {
+                    echo $user['description'];
+                } ?></textarea>
             </div>
         </form>
     </div>
--- updateProfile.php
+++ updateProfile.php
@@ -4,23 +4,25 @@ require_once 'config.php';
 
 ?>
 <div class="container">
-    <button type="button" class="btn-back" id="back" onclick="window.history.back()"><ion-icon name="chevron-back-outline"></ion-icon> Kembali</button>
+    <button type="button" class="btn-back" id="back" onclick="window.history.back()"><ion-icon
+            name="chevron-back-outline"></ion-icon> Kembali</button>
     <div class="profile" id="profile">
-    <div class="user-profile user-profile-edit">
-        <img src="assets/imgs/customer01.jpg" alt="">
-        <ion-icon name="pencil-sharp" class="pencil"></ion-icon>
-    </div>
-    
-    <form action="" method="POST">
-        <div class="add-input add-input-profile">
-            <input type="file" name="gambar" style="display:none;" id="inputGambar">
-            <input type="text" name="firstname" placeholder="Firstname" autofocus>
-            <input type="text" name="lastname" placeholder="Lastname">
-            <input type="text" name="username" placeholder="Username">
-            <input type="text" name="hobbies" placeholder="Hobbies">
-            <textarea type="text" placeholder="Description" name="body"></textarea>
+        <div class="user-profile user-profile-edit">
+            <img src="assets/imgs/customer01.jpg" alt="">
+            <ion-icon name="pencil-sharp" class="pencil"></ion-icon>
         </div>
-    </form>
+
+        <form action="" method="POST">
+            <div class="add-input add-input-profile">
+                <input type="file" name="gambar" style="display:none;" id="inputGambar">
+                <input type="text" name="firstname" placeholder="Firstname" autofocus>
+                <input type="text" name="lastname" placeholder="Lastname">
+                <input type="text" name="username" placeholder="Username">
+                <input type="email" name="email" placeholder="Email">
+                <input type="text" name="hobbies" placeholder="Hobbies">
+                <textarea type="text" placeholder="Description" name="body"></textarea>
+            </div>
+        </form>
     </div>
 </div>
 <?php require_once 'templates/footer.php'; ?>
